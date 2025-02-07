6968ff4b0b00c1b50f2e4b871131de86dfca3054
fodare <folorunsodamilare007@hotmail.com>
2025-02-07 17:59:18 +0100

    Initial maui template files and nesting of old data from python tkinter implementation.

---
+++ flashcard/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace flashcard;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
