7b0a00fd6dc28883e0e55236b4f048ee5cb52030
fodare <folorunsodamilare007@hotmail.com>
2025-02-14 11:18:07 +0100

    Remove shared list and update write to file logic.

--- flashcard/Services/LanguageService.cs
+++ flashcard/Services/LanguageService.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics;
 using System.Text.RegularExpressions;
+using Android.Media;
 using flashcard.Model;
+using Javax.Crypto.Spec;
 
 namespace flashcard.Services
 {
@@ -8,13 +11,12 @@ namespace flashcard.Services
         private static readonly string appDirectory = FileSystem.Current.AppDataDirectory;
         private static readonly string WordsToLearnDirectory = System.IO.Path.Combine(appDirectory, "wordsToLearn.csv");
 
-        private readonly List<LanguageModel> Words = new();
-
         public async Task<List<LanguageModel>> ReadWordsFromDataFile()
         {
             using var fileStream = await FileSystem.OpenAppPackageFileAsync("language_data.csv");
             using StreamReader? reader = new(fileStream);
             string line;
+            List<LanguageModel> wordsList = new();
             while ((line = reader.ReadLine()!) != null)
             {
                 //Define pattern
@@ -22,32 +24,55 @@ namespace flashcard.Services
 
                 //Separating columns to array
                 string[] parsedRow = CSVParser.Split(line);
-                Words?.Add(new LanguageModel { ForeignFormat = parsedRow[0], EnglishFormat = parsedRow[1] });
+                wordsList.Add(new LanguageModel { ForeignFormat = parsedRow[0], EnglishFormat = parsedRow[1] });
             }
-            return Words!;
+            return wordsList!;
         }
 
         public async Task<List<LanguageModel>> ReadWordsToLearn()
         {
             using StreamReader? streamReader = new(WordsToLearnDirectory);
             string? line;
+            List<LanguageModel> wordsToLearnList = new();
             while ((line = await streamReader.ReadLineAsync()!) != null)
             {
                 //Define pattern
                 Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
                 //Separating columns to array
                 string[] parsedRow = CSVParser.Split(line);
-                Words?.Add(new LanguageModel { ForeignFormat = parsedRow[0], EnglishFormat = parsedRow[1] });
+                wordsToLearnList.Add(new LanguageModel { ForeignFormat = parsedRow[0], EnglishFormat = parsedRow[1] });
             }
-            return Words;
+            return wordsToLearnList;
+        }
+
+        public async Task<bool> WordInWordsToLearn(string foreignWord)
+        {
+            var wordsList = await ReadWordsToLearn();
+            LanguageModel? word = wordsList.FirstOrDefault(word => word.ForeignFormat == foreignWord);
+            if (word is null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
         public async Task AddToWordsToLearn(LanguageModel newWord)
         {
-            using StreamWriter streamWriter = new(WordsToLearnDirectory);
             var newLine = string.Format("{0},{1}", newWord.ForeignFormat, newWord.EnglishFormat);
-            await streamWriter.WriteLineAsync(newLine);
-            streamWriter.Flush();
+            using StreamWriter streamWriter = new(WordsToLearnDirectory, true);
+
+            if (!await WordInWordsToLearn(newWord.ForeignFormat!))
+            {
+                await streamWriter.WriteLineAsync(newLine);
+                streamWriter.Flush();
+            }
+            else
+            {
+                Debug.WriteLine($"{newWord.ForeignFormat} already in words_to_learn_list");
+            }
         }
     }
 }
